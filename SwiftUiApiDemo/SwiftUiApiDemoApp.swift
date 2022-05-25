77965c21edb1a27020c9ecd39c13f6f9689642e7
Emmanuel Cruz <emmanuelcruz.personal@gmail.com>
2022-05-24 22:12:59 -0500

    Initial Commit

---
+++ SwiftUiApiDemo/SwiftUiApiDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiApiDemoApp.swift
+//  SwiftUiApiDemo
+//
+//  Created by emmanuel.cruz on 24/05/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiApiDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
